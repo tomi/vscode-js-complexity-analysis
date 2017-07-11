fcdd1eba29684c6c9ea7c4f54c2c0b50cde27d2e
Tomi Turtiainen <tomi.turtiainen@gmail.com>
2017-07-11 16:48:49 +0300

    Use typjonjs-escomplex to analyse the project

--- src/complexity-analyzer.js
+++ src/complexity-analyzer.js
@@ -1,29 +1,14 @@
 "use strict";
 
-const esprima   = require("esprima");
-const walker    = require("escomplex-ast-moz");
-const escomplex = require("escomplex");
 const utils     = require("./utils");
-
-function parseJs(js) {
-    try {
-        return esprima.parse(js, { sourceType: "script", loc: true, jsx: true });
-    } catch (error) {
-        return esprima.parse(js, { sourceType: "module", loc: true, jsx: true });
-    }
-}
+const escomplex = require("typhonjs-escomplex");
 
 function analyse(js) {
-    const ast = parseJs(js);
-    const result = escomplex.analyse(ast, walker);
-
-    return result;
+    return escomplex.analyzeModule(js);
 }
 
 function process(analyses) {
-    const summary = escomplex.processResults({
-        reports: analyses
-    }, false);
+    const summary = escomplex.processProject(analyses);
 
     summary.totalLOC = utils.sum(summary.reports.map(report =>
         report.aggregate.sloc.logical));
--- src/models/file-analysis.js
+++ src/models/file-analysis.js
@@ -8,21 +8,21 @@ const utils = require("./../utils");
 function FileAnalysis(path, analysis) {
     this.path = path;
 
-    const cyclomatic = analysis.functions.map(f => f.cyclomatic);
+    this.dependencies = analysis.dependencies;
 
     // Scale to between 0 and 100
     this.maintainability = Math.max(0, analysis.maintainability * 100 / 171);
-    this.sloc            = analysis.aggregate.sloc.logical;
+    this.sloc            = analysis.methodAggregate.sloc.logical;
     this.cyclomatic      = {
-        avg: utils.average(cyclomatic),
-        max: utils.max(cyclomatic)
+        avg: analysis.methodAverage.cyclomatic,
+        max: analysis.methodAggregate.cyclomatic
     };
-    this.difficulty      = analysis.aggregate.halstead.difficulty
-    this.bugs            = analysis.aggregate.halstead.bugs;
+    this.difficulty = analysis.methodAggregate.halstead.difficulty
+    this.bugs       = analysis.methodAggregate.halstead.bugs;
 
-    this.functions = analysis.functions.map(f => ({
+    this.functions = analysis.methods.map(f => ({
         name:       f.name,
-        line:       f.line,
+        line:       f.lineStart,
         params:     f.params,
         sloc:       f.sloc.logical,
         cyclomatic: f.cyclomatic,
